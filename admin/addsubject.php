5fbc6084bc236d880e5fc4cb8e172372fd2562e6
AdeelAtta <adeelatta2000@gmail.com>
2022-08-09 13:50:14 +0500

    changes to database and add subject page

--- admin/addsubject.php
+++ admin/addsubject.php
@@ -1,22 +1,21 @@
 <?php
-if(isset($_POST['subName']) && isset($_POST['subUniId']) &&  isset($_POST['subTestId'])){
+if(isset($_POST['subName']) &&  isset($_POST['subTestId'])){
 
 
 $subName  = $_POST['subName'];
-$subUniId = $_POST['subUniId'];
 $subTestId = $_POST['subTestId'];
 include 'db.php';
-$query  = "SELECT * FROM subject WHERE sub_name LIKE '%$subName%' AND sub_uni_Id = $subUniId AND sub_test_id = $subTestId ";
+$query  = "SELECT * FROM subject WHERE sub_name LIKE '%$subName%' AND  sub_test_id = $subTestId ";
 $result = mysqli_query($connection,$query);
 if(mysqli_num_rows($result) > 0){
-    $subName = $subUniId = $subTestId = null;
+    $subName =  $subTestId = null;
     header( "Location: ../adminpanel.php?subjectAlreadyExist" );
 
 }else{
 
-    $query = "INSERT INTO subject(sub_name,sub_uni_id,sub_test_id) VALUES('{$subName}',{$subUniId},{$subTestId})";
+    $query = "INSERT INTO subject(sub_name,sub_test_id) VALUES('{$subName}',{$subTestId})";
     if(mysqli_query($connection,$query)){
-        $subName = $subUniId = $subTestId = null;
+        $subName =  $subTestId = null;
         header( "Location: ../adminpanel.php?subjectAdded" );
     }else{
         echo mysqli_error($connection);
@@ -27,5 +26,5 @@ if(mysqli_num_rows($result) > 0){
 
 
 }else{
-    header( "Location: ../adminpanel.php?aaa" );
+    header( "Location: ../adminpanel.php" );
 } ?>
--- adminpanel.php
+++ adminpanel.php
@@ -33,42 +33,8 @@
     <main class="main-content" >
 
 
-      <!-- <section class="section pt-10">
-        <div class="container">
-          <header class="section-header">
-            <h2 class="display-4">Admin Panel</h2>
-            <hr>
-          </header>
-
-          <form class="form-row" action="action.php" method="POST" enctype="multipart/form-data">
-            <div class="col-12">
-                <div class="alert alert-success d-on-success">We received your message and will contact you back soon.</div>
-            </div>
-
-            <div class="form-group col-sm-6 col-xl-3">
-              <input class="form-control form-control-lg" type="text" name="topicName" placeholder="Topic Name">
-            </div>
-
-            <div class="form-group col-12">
-              <textarea id="Article_editor" class="form-control form-control-lg" rows="4" placeholder="Message" name="topicData"></textarea>
-            </div>
-
-            <div class="col-12 text-center">
-              <button class="btn btn-xl btn-block btn-primary" type="submit">Submit</button>
-            </div>
-          </form>
-        <script src="ckeditor/ckeditor.js"></script>
-        <script>
-            CKEDITOR.replace('Article_editor');
-        </script>
-        </div>
-      </section> -->
-
-
-
-
-
       <!-- Add University -->
+      
       <section class="section p-5 mt-10 " >
         <div class="container">
           <form class="form-row "  action="admin/adduniversity.php" method="POST" enctype="multipart/form-data">
@@ -99,8 +65,7 @@
       </section>
       <hr class="border border-primary border-2 opacity-50">
 
-      
-
+    
       <!-- Add Tests -->
       <section class="section p-5 mt-5 ">
         <div class="container">
@@ -141,10 +106,7 @@
       <hr class="border border-primary border-2 opacity-50">
 
 
-
-
-
-            <!-- Add Subject -->
+      <!-- Add Subject -->
       <section class="section p-5 mt-5 ">
         <div class="container">
           <form class="form-row" action="admin/addsubject.php" method="POST" enctype="multipart/form-data">
@@ -165,30 +127,22 @@
               <input class="form-control form-control-lg" type="text" name="subName" placeholder="Subject Name">
             </div>
 
-            <!-- select university -->
+            <!-- UNI & test type -->
             <div class="form-group col-sm-6 col-xl-3">
-            <select name="subUniId" id="uni" class="form-control form-control-lg">
+            <select name="subTestId" id="uni" class="form-control form-control-lg">
               <?php 
               $query = "Select * from university";
               $result = mysqli_query($connection,$query);
-              while($row = mysqli_fetch_array($result)){ ?>
-               <option value="<?php echo $row['uni_id']; ?>"><?php echo $row['uni_name']; ?></option>
-              <?php  } ?>   
+              while($row = mysqli_fetch_array($result)){
+                $quer = "SELECT * FROM test WHERE test_uni_id = {$row['uni_id']} ";
+                $res = mysqli_query($connection,$quer);
+                while($test = mysqli_fetch_array($res)){ ?>
+                 <option value="<?php echo $test['test_id']; ?>"><?php echo $row['uni_name'].'('.$test['test_name'].')'; ?></option>
+               <?php } } ?>   
             </select>
             </div>
 
 
-                <!-- select test type -->
-            <div class="form-group col-sm-6 col-xl-3">
-            <select name="subTestId" id="" class="form-control form-control-lg">
-              <?php 
-              $query = "Select * from test";
-              $result = mysqli_query($connection,$query);
-              while($row = mysqli_fetch_array($result)){ ?>
-               <option value="<?php echo $row['test_id']; ?>"><?php echo $row['test_name']; ?></option>
-              <?php  } ?>   
-            </select>
-            </div>
            
             <div class="form-group col-sm-6 col-xl-3 text-center">
               <button class="btn btn-xl btn-block btn-primary" type="submit">Add Subject</button>
@@ -198,18 +152,68 @@
       </section>
       <hr class="border border-primary border-2 opacity-50">
       
+      
+
+
+       <!-- Add Topic/Article -->
+      <section class="section p-5 mt-5 ">
+        <div class="container">
+          <form class="form-row" action="admin/addtopic.php" method="POST" enctype="multipart/form-data">
+          <legend>Add Topic</legend> 
+          <?php if(isset($_GET['topicAdded'])){ ?> 
+          <div class="col-12">
+                <div class="alert alert-success d-on-success">Topic Added Successfully</div>
+            </div>
+          <?php }elseif(isset($_GET['topicAlreadyExist'])) {?> 
+          <div class="col-12">
+            <div class="alert alert-danger d-on-success">Topic Already Exist</div>
+          </div>
+        <?php  } ?>
+
+
+          <!-- Topic Name -->
+            <div class="form-group col-sm-6 col-xl-3">
+              <input class="form-control form-control-lg" type="text" name="topicName" placeholder="Topic Name">
+            </div>
+
+            <!-- UNI & test type -->
+            <div class="form-group col-sm-6 col-xl-3">
+            <select name="subUniId" id="uni" class="form-control form-control-lg">
+              <?php 
+              $query = "Select * from university";
+              $result = mysqli_query($connection,$query);
+              while($row = mysqli_fetch_array($result)){
+                $quer = "SELECT * FROM test WHERE test_uni_id = {$row['uni_id']} ";
+                $res = mysqli_query($connection,$quer);
+                while($test = mysqli_fetch_array($res)){ 
+                  
+                  $q = "SELECT * FROM subject WHERE sub_test_id = {$test['test_id']} " ;
+                  $r = mysqli_query($connection,$q);
+                  while($sub = mysqli_fetch_array($r)){  ?>
+                 <option value="<?php echo $sub['sub_id']; ?>"><?php echo $sub['sub_name'].' '.$row['uni_short'].'('.$test['test_name'].')'; ?></option>
+               <?php } } } ?>   
+            </select>
+            </div>
+
+
+            <div class="form-group col-12">
+              <textarea id="Article_editor" class="form-control form-control-lg" rows="4" placeholder="Message" name="topicData"></textarea>
+            </div>
 
 
 
-
-
-
-
-
-
-
-
-
+           
+            <div class="col-12 text-center">
+              <button class="btn btn-xl btn-block btn-primary" type="submit">ADD Topic</button>
+            </div>
+          </form>
+          <script src="ckeditor/ckeditor.js"></script>
+        <script>
+            CKEDITOR.replace('Article_editor');
+        </script>
+        </div>
+      </section>
+      <hr class="border border-primary border-2 opacity-50">
 
 
 
